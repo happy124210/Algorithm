71982b53e72becd52014e74da4608249a01be8cd
윤아 <142162564+ria1211@users.noreply.github.com>
2024-11-11 00:19:54 +0900

    [Bronze II] Title: 벌집, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -0,0 +1,13 @@
+def Beehouse(N):
+    num = (N-2)//6
+    for i in range(1, 1000000000):
+        numRange = i*(i-1)/2
+        if num < numRange:
+            print(i)
+            break
+
+N = int(input())
+if N == 1:
+    print(1)
+else:
+    Beehouse(N)
