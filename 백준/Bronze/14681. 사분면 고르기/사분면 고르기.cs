aae287c59499031a10e454b013969239cf26e631
윤아 <142162564+happy124210@users.noreply.github.com>
2025-04-16 21:59:38 +0900

    [Bronze V] Title: 사분면 고르기, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cs
@@ -0,0 +1,15 @@
+// 14681. 사분면 고르기
+
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0)
+{
+    if (y > 0) Console.Write(1);
+    else Console.Write(4);
+}
+else
+{
+    if (y > 0) Console.Write(2);
+    else Console.Write(3);
+}
