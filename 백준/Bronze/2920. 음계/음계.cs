c51cea91af65a13fb5b6a6e569fa98340a10b838
윤아 <142162564+happy124210@users.noreply.github.com>
2025-04-28 20:56:22 +0900

    [Bronze II] Title: 음계, Time: 48 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.cs
@@ -0,0 +1,6 @@
+//2920. 음계
+
+string input = Console.ReadLine();
+if (input == "1 2 3 4 5 6 7 8") Console.Write("ascending");
+else if (input == "8 7 6 5 4 3 2 1") Console.Write("descending");
+else Console.Write("mixed");
